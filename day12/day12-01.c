784f48ead5b59fe6e7b15bf58211af164a6e49d2
leeleeleeleejun <wnstjr6293@naver.com>
2024-12-01 22:47:10 +0900

    Update day12-01.c

--- day12/day12-01.c
+++ day12/day12-01.c
@@ -6,32 +6,27 @@
 // topping_len은 배열 topping의 길이입니다.
 int solution(int topping[], size_t topping_len) {
 
-    int left_tops[MAX_TOPPING + 1] = { 0 }; // 철수가 먹는 토핑
-    int right_tops[MAX_TOPPING + 1] = { 0 }; // 동생이 먹는 토핑
-    int num_left = 0;  // 철수가 먹는 토핑 가지수
-    int num_right = 0; // 동생이 먹는 토핑 가지수
-    int num_fairs = 0; // 공평한 경우의 수
+    int left_tops[MAX_TOPPING + 1] = { 0 };
+    int right_tops[MAX_TOPPING + 1] = { 0 }; 
+    int num_left = 0; 
+    int num_right = 0; 
+    int num_fairs = 0;
 
-    // 동생이 모두 먹을 때 초기화
     for (int i = 0; i < topping_len; i++) {
         int top = topping[i];
-        if (right_tops[top] == 0) num_right++; // 동생이 먹는 토핑 가지수 증가
+        if (right_tops[top] == 0) num_right++;
         right_tops[top]++;
     }
 
-    // 철수가 왼쪽부터 하나씩 토핑을 가져감
     for (int i = 0; i < topping_len; i++) {
         int top = topping[i];
 
-        // 동생의 토핑 관리
-        right_tops[top]--; // 동생이 하나 뺌
-        if (right_tops[top] == 0) num_right--; // 동생이 먹는 토핑 가지수 감소
+        right_tops[top]--;
+        if (right_tops[top] == 0) num_right--;
 
-        // 철수의 토핑 관리
-        if (left_tops[top] == 0) num_left++; // 철수가 먹는 토핑 가지수 증가
+        if (left_tops[top] == 0) num_left++;
         left_tops[top]++;
 
-        // 공평한 경우인지 확인
         if (num_left == num_right) num_fairs++;
     }
 
